1803ed5e8f03c5ffd55b727b17033b98d4f82c19
Taynus <poncevelez.l@northeastern.edu>
2026-01-12 18:19:02 -0800

    temperature
    change the body

---
+++ lab1/temp-conversion.js
@@ -0,0 +1,7 @@
+let fahrenheit = 77 // 0c - 273.15k //Lima april 2025
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Temperature (celsius):" + celsius)
+console.log("Temperature (kelvin):" + kelvin)
+console.log("Temperature (fahrenheit):" + fahrenheit)
